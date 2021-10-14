11fc92e78e294b49d3f11da7e8e6e74b8c97c1c9
Sohel Ahmed Jony <jony@debian-BULLSEYE-live-builder-AMD64>
2021-10-14 22:52:36 +0600

    Adding Library for VCS. Named vcs_lib.h

---
+++ vcs_lib.h
@@ -0,0 +1,11 @@
+int vcs_osname()
+{
+    // not detected = 0, windows = 1, linux = 2;
+    return osname;
+}
+
+int check_compiler()
+{
+    // no_comiler = 0, gcc = 1, clang = 2,
+    return 1;
+}

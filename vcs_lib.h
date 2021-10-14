be951a92de82a341b91646825fc3003fe309abf6
nishattasnin <nishattasnin02@gmail.com>
2021-10-15 01:49:33 +0600

    Added a start print function

--- vcs_lib.h
+++ vcs_lib.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+
 int vcs_osname()
 {
     // not detected = 0, windows = 1, linux = 2;
@@ -6,6 +9,30 @@ int vcs_osname()
 
 int check_compiler()
 {
-    // no_comiler = 0, gcc = 1, clang = 2,
+    // no_comiler = 0, gcc = 1, clang = 2, mcc = 3
     return 1;
+}
+
+int Strat_print()
+
+{
+    // Printing OS info
+    char os_name[20];
+    int os_code = vcs_osname();
+    if (os_code == 1)
+    {
+        strcpy(os_name, "Windows");
+    }
+    else if (os_code == 2)
+    {
+        strcpy(os_name, "Linux");
+    }
+    else if (os_code == 0)
+    {
+        strcpy(os_name, "Unable to detected");
+    }
+    printf("OS: %s\n", os_name);
+    // Printing Compiler info
+
+    return 0;
 }

36ea38dcc773db234caa1a11ea05e204c0af8c38
nishattasnin <nishattasnin02@gmail.com>
2021-10-19 01:32:55 +0600

    added the compiler info

--- vcs_lib.h
+++ vcs_lib.h
@@ -14,7 +14,7 @@ int check_compiler()
 }
 int check_vscode()
 {
-    //1 for vscode is installed and 0 for vscode not installed
+    // 1 for vscode is installed and 0 for vscode not installed
     return 1;
 }
 int check_os_architechture()
@@ -44,5 +44,24 @@ int Start_print()
     printf("OS: %s\n", os_name);
     // Printing Compiler info
 
+    char compiler_name[30];
+    int compiler_code = check_compiler();
+    if (compiler_code == 1)
+    {
+        strcpy(compiler_name, "gcc");
+    }
+    else if (compiler_code == 2)
+    {
+        strcpy(compiler_name, "clang");
+    }
+    else if (compiler_code == 3)
+    {
+        strcpy(compiler_name, "mcc"); //
+    }
+    else if (compiler_code == 0)
+    {
+        strcpy(compiler_name, "Unable to be detected"); // Name formatting korte hobe
+    }
+    printf("Compiler: %s\n", compiler_name); //
     return 0;
 }

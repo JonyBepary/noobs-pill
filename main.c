4781683d7d67c31430617e5535fc19eebaf7f8e6
Sohel Ahmed Jony <sohelahmedjony@gmail.com>
2021-11-23 01:14:30 +0600

    cleaned

--- main.c
+++ main.c
@@ -4,28 +4,14 @@
 int main(int argc, char const *argv[])
 {
 
-    //
+
     calibrate();
+    // clear screen Start_print
     clear_screen();
     Start_print();
-    // puts(getenv("PATH"));
-
-    // printf("%d", system("gcc main.c -lurlmon -o main2"));
-    // if (ret != 0)
-    // {
-    //     printf("tool is not here, move along\n");
-    // }
-    // if (system("wget"))
-    // {
-    // }
 
     // Download_Wget();
-
-    // printf("sys: %d\n", );
-
     // Download_LIST_Through_wget();
-
-    // printf("%s", urlencode("Hello / world ="));
 
     return 0;
 }

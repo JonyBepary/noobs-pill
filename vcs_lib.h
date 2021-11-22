bfda8e7f8b3fd7a98a6169f01545aa73d655c66f
Sohel Ahmed Jony <sohelahmedjony@gmail.com>
2021-11-22 23:19:41 +0600

    Added check_compiler() and calibration function

--- vcs_lib.h
+++ vcs_lib.h
@@ -14,25 +14,20 @@
 #endif
 
 #ifdef __i386__
-#define OS_architechture 1
+#define sys_architechture 1
 #elif __x86_64__
-#define OS_architechture 2
+#define sys_architechture 2
 #elif __aarch64__
-#define OS_architechture 4
+#define sys_architechture 4
 #elif __arm__
-#define OS_architechture 3
+#define sys_architechture 3
 // do arm stuff
 #endif
 
-int check_compiler()
+void calibrate()
 {
-    // no_comiler = 0, gcc = 1, clang = 2, mcc = 3
-    return 1;
-}
-int check_vscode()
-{
-    // 1 for vscode is installed and 0 for vscode not installed
-    return 1;
+    check_compiler();
+    check_vscode();
 }
 
 int User_command()
@@ -56,32 +51,13 @@ int Start_print()
     {
         strcpy(os_name, "Unable to detected");
     }
-    printf("OS: %s\n", os_name);
-
+    print_os_name();
     // Printing Compiler info
     char compiler_name[30];
-    int compiler_code = check_compiler();
-    if (compiler_code == 1)
-    {
-        strcpy(compiler_name, "gcc");
-    }
-    else if (compiler_code == 2)
-    {
-        strcpy(compiler_name, "clang");
-    }
-    else if (compiler_code == 3)
-    {
-        strcpy(compiler_name, "mcc");
-    }
-    else if (compiler_code == 0)
-    {
-        strcpy(compiler_name, "Unable to be detected");
-    }
-    printf("Compiler: %s\n", compiler_name);
-    // OS_architechture: 1 for x86, 2 for x64 , 3 for AArch32
+    // sys_architechture: 1 for x86, 2 for x64 , 3 for AArch32
     //  and 4 for AArch64
-    printf("OS Archtitechture: ");
-    switch (OS_architechture)
+    printf("System Archtitechture: ");
+    switch (sys_architechture)
     {
     case 1:
         printf("x86\n");
@@ -99,5 +75,23 @@ int Start_print()
         printf("Undefined\n");
         break;
     }
+    check_compiler();
+    for (size_t i = 0; i < 4; i++)
+    {
+        if (c_compiler[i] == 1)
+        {
+            printf("%s Installed\n", c_compiler_name[i]);
+        }
+    }
+    if (vscode_editor == 1)
+    {
+        printf("Visual studio code installed\n");
+    }
+    if (check_config() == -1)
+    {
+        printf("Config not found!\n");
+        /* code */
+    }
+
     return 0;
 }

20e34a4391d64367734520dad479edf4f06bd473
Sohel Ahmed Jony <sohelahmedjony@gmail.com>
2021-11-22 23:58:02 +0600

    Added MAin.c

---
+++ main.c
@@ -0,0 +1,31 @@
+// -lurlmon
+#include "vcs_lib.h"
+
+int main(int argc, char const *argv[])
+{
+
+    //
+    calibrate();
+    clear_screen();
+    Start_print();
+    // puts(getenv("PATH"));
+
+    // printf("%d", system("gcc main.c -lurlmon -o main2"));
+    // if (ret != 0)
+    // {
+    //     printf("tool is not here, move along\n");
+    // }
+    // if (system("wget"))
+    // {
+    // }
+
+    // Download_Wget();
+
+    // printf("sys: %d\n", );
+
+    // Download_LIST_Through_wget();
+
+    // printf("%s", urlencode("Hello / world ="));
+
+    return 0;
+}

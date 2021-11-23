4ce2632b253f5d152c4bd984fa282db055e5e08c
Sohel Ahmed Jony <sohelahmedjony@gmail.com>
2021-11-23 19:49:34 +0600

    Added Text Colorizer

--- vcs_lib.h
+++ vcs_lib.h
@@ -24,24 +24,46 @@
 // do arm stuff
 #endif
 
+
 void calibrate()
 {
     check_compiler();
     check_vscode();
+    check_wget();
 }
-
+void print_sign()
+{
+    printf("[");          // Display Hello in red
+    printf("\033[0;32m"); // Set the text to the color red
+    printf("✓");          // Display Hello in red
+    printf("\033[0;0m");  // Set the text to the color red
+    printf("] ");         // Display Hello in red
+}
+void print_cross()
+{
+    printf("[");          // Display Hello in red
+    printf("\033[0;31m"); // Set the text to the color red
+    printf("x");          // Display Hello in red
+    printf("\033[0;0m");  // Set the text to the color red
+    printf("]");          // Display Hello in red
+}
 int User_command()
 {
     return 1;
 }
-int Start_print()
+int print_system_info()
 {
+    printf("==========================================\n");
+    printf("             NOOBS PILL                       \n");
+    printf("==========================================\n");
     // Printing OS info
+
     print_os_name();
     // Printing Compiler info
     char compiler_name[30];
     // sys_architechture: 1 for x86, 2 for x64 , 3 for AArch32
     //  and 4 for AArch64
+    print_sign();
     printf("System Archtitechture: ");
     switch (sys_architechture)
     {
@@ -62,31 +84,41 @@ int Start_print()
         break;
     }
 
-    //printing  all installed c compiler
+    // printing  all installed c compiler
     for (size_t i = 0; i < 4; i++)
     {
         if (c_compiler[i] == 1)
         {
+            print_sign();
             printf("%s Installed\n", c_compiler_name[i]);
         }
     }
 
-    
+    // print_sign();
     if (vscode_editor == 1)
     {
+        print_sign();
         printf("Visual studio code installed\n");
     }
     else
     {
+        print_cross();
         printf("Visual studio code not installed!\n");
-        /* code */
     }
 
     if (check_config() == -1)
     {
-        printf("Config not found!\n");
+        print_cross();
+        printf("System is not configured!\n");
         /* code */
     }
+    else
+    {
+        print_sign();
+        printf("System configured!\n");
+    }
 
+    // print_cross();
+    // print_sign();
     return 0;
 }

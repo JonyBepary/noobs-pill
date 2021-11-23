11843d213b9671288a57b8da9c61e41089cb2ce8
Sohel Ahmed Jony <sohelahmedjony@gmail.com>
2021-11-23 19:50:38 +0600

    Added Construct Download list function

--- main.c
+++ main.c
@@ -5,12 +5,17 @@ int main(int argc, char const *argv[])
 {
 
     calibrate();
-
     clear_screen();
+    print_system_info();
+    // show_command();
+    // if (wget_av == 0)
+    // {
+    // }
+    // // Download_LIST_Through_wget();
+    check_oscode();
+    construct_download_list();
+    // printf("%s", OS_CODE);
 
-    Start_print();
-
-    // Download_Wget();
     // Download_LIST_Through_wget();
 
     return 0;

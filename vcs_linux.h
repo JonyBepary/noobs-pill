5b54839089fd1d69641f8b7442087bbdc80732da
Sohel Ahmed Jony <sohelahmedjony@gmail.com>
2021-12-04 23:21:41 +0600

    add fixer

--- vcs_linux.h
+++ vcs_linux.h
@@ -140,7 +140,7 @@ void print_os_name()
 // welcome banner
 void printbaner()
 {
-    system("./packages/lolcat -r ./script/art");
+    system("./packages/lolcat -r ./script/art  2>/dev/null");
 }
 // task banner
 void printbaner2()

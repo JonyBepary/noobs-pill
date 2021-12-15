a5f4cd904ab0253d27719bb86445cc5e6633d6fb
Sohel Ahmed Jony <sohelahmedjony@gmail.com>
2021-12-15 13:45:02 -0800

    Setting and improved script

--- script/install.bat
+++ script/install.bat
@@ -1,2 +1,4 @@
+echo Installing Gnu c Compiler and over Devel Package 
+echo just press enter  
 pacman -Syuu
 pacman -S base-devel mingw-w64-x86_64 mingw-w64-x86_64-toolchain
--- script/plugin.bat
+++ script/plugin.bat
@@ -1,6 +1,7 @@
+echo installing Visual studio plugin
+echo
 code --install-extension formulahendry.code-runner
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension ms-vscode.cpptools
-code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension Equinusocio.vsc-community-material-theme
 code --install-extension vscode-icons-team.vscode-icons
